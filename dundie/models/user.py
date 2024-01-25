61040b4e1c4fa41db44b59ce87fcaf6f76edfd8e
Leonardo Pereira <ethical.leonardo@gmail.com>
2024-01-25 14:50:35 -0300

    Criado secret key e configurado hash das senhas

--- dundie/models/user.py
+++ dundie/models/user.py
@@ -1,6 +1,7 @@
 """User related data models"""
 from typing import Optional
 from sqlmodel import Field, SQLModel
+from dundie.security import HashedPassword
 
 
 class User(SQLModel, table=True):
@@ -11,7 +12,7 @@ class User(SQLModel, table=True):
     username: str = Field(unique=True, nullable=False)
     avatar: Optional[str] = None
     bio: Optional[str] = None
-    password: str = Field(nullable=False)
+    password: HashedPassword
     name: str = Field(nullable=False)
     dept: str = Field(nullable=False)
     currency: str = Field(nullable=False)
@@ -20,3 +21,13 @@ class User(SQLModel, table=True):
     def superuser(self):
         """Users belonging to management dept are admins."""
         return self.dept == "management"
+
+
+def generate_username(name: str) -> str:
+    """Generate a slug drom user.name.
+    "Tiago Lemos" -> "tiago-lemos"
+    "Luan Oliveira" -> "luan-oliveira"
+    # TODO: Use library slugify
+    """
+
+    return name.lower().replace(" ", "-")
