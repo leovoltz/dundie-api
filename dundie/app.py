12703f1ecbfff5122da9c9ab1b1abee4c1147cdc
Leonardo Pereira <ethical.leonardo@gmail.com>
2024-05-20 23:08:16 -0300

    Criado Depends no dp.py e realizado teste da dependencia no app.py com rota simples

--- dundie/app.py
+++ dundie/app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from dundie.db import ActiveSession
 
 app = FastAPI(
     title="dundie",
     version="0.1.0",
     description="dundie is a rewards API",
 )
+
+
+from dundie.models import User
+from sqlmodel import Session, select
+from dundie.models.user import UserResponse
+
+
+@app.get("/", response_model=UserResponse)
+def hello(session: Session = ActiveSession):
+    return session.exec(select(User)).first
